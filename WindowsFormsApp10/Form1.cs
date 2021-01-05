e23d86ed2fe567a9dd37a0d27d4cfb8950c4513b
baghiz zuhdi adzin <baghizzhd@gmail.com>
2021-01-05 15:12:51 +0700

    Add project files.

---
+++ WindowsFormsApp10/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10
+{
+    public partial class FormGomoku : Form
+    {
+        public FormGomoku()
+        {
+            InitializeComponent();
+        }
+        private void StretchPic()
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
